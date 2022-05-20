470e4597d58f006c20383eb6d33f23ded580099f
= <=>
2022-05-19 23:48:25 -0700

    bug fixes in custom commands

--- finance/trader/management/commands/update_daily_stock_market_data.py
+++ finance/trader/management/commands/update_daily_stock_market_data.py
@@ -1,6 +1,8 @@
+import traceback
 import yfinance as yf
 from trader.models import TickerInfo, DailyStockMarketData
 from django.core.management.base import BaseCommand, CommandError
+from multiprocessing import Pool
 from datetime import date, datetime
 import time
 
@@ -18,13 +20,14 @@ class Command(BaseCommand):
             df = yf.download(tickers=tickers, period='1d', interval='1d', group_by='ticker')
             for ticker, data in df.groupby(level=0, axis=1):
                 entry = DailyStockMarketData(ticker=ticker,
-                                             date=data.index.to_pydatetime()[0].date(),
-                                             open=data[ticker]['Open'],
-                                             close=data[ticker]['Close'],
-                                             adj_close=data[ticker]['Adj Close'],
-                                             volume=data[ticker]['Volume'])
+                                             date=data.index[0].date(),
+                                             open=data[ticker]['Open'][0],
+                                             close=data[ticker]['Close'][0],
+                                             adj_close=data[ticker]['Adj Close'][0],
+                                             volume=data[ticker]['Volume'][0])
                 entry.save()
             self.stdout.write(self.style.SUCCESS('Successfully updated the daily stock market table'))
         except Exception:
+            traceback.print_exc()
             raise CommandError('unable to update daily stock market data table')
 
