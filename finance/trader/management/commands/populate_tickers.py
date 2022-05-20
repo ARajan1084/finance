5ef794293a924ff1736ecb44fa500a14aecb4a9e
= <=>
2022-05-19 23:46:59 -0700

    added commands for populating ticker table and updating the daily stock market data through yfinance

---
+++ finance/trader/management/commands/populate_tickers.py
@@ -0,0 +1,22 @@
+import traceback
+
+import pandas as pd
+from trader.models import TickerInfo
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        try:
+            df = pd.read_csv('trader/management/commands/sp500.csv')
+            for index, row in df.iterrows():
+                ticker = TickerInfo(ticker=row['Symbol'], security=row['Security'], sector=row['GICS Sector'])
+                ticker.save()
+            self.stdout.write(self.style.SUCCESS('Successfully populated ticker info table'))
+        except Exception as e:
+            traceback.print_exc()
+            raise CommandError('Unable to populate ticker info table')
+
